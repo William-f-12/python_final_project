667f4bf8783779d61848ac26a71b63987f9a86e4
William-f-12 <3326007521@qq.com>
2021-05-21 12:01:57 +0800

    finish fibonacci series in Little_Game.py

--- Little_Game.py
+++ Little_Game.py
@@ -5,6 +5,21 @@ class Series():
     def __init__(self):
         self.list = []
 
-    def Fibonacci(self):
+    def Fibonacci(self, position: int):
+        """position: need to be an integer greater than 0, return first # numbers of the series"""
+
+        if type(position) != int or position < 1:
+            try:
+                raise ValueError("position need to be an integer greater than 0")
+            except Exception as e:
+                print(e)
+                return 0
+
         f = [1,1]
-        
+        if position < 3:
+            return f[:position]
+
+        while len(f) < position:
+            new = f[-1] + f[-2]
+            f.append(new)
+        return f[:position]
