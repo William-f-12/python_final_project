c6f194dee9426960dc13a85cddb2f2544e67486d
William Lu <3326007521@qq.com>
2021-05-26 21:30:56 -0500

    add one more function to Little_Game.py

--- Little_Game.py
+++ Little_Game.py
@@ -19,7 +19,8 @@ class Series():
         self.f = self.Fibonacci(position+1)
         self.g = self.Geo(position+1)
         self.m = self.Alt(position+1)
-        self.list = [self.f, self.g, self.m]
+        self.s = self.Sum(position+1)
+        self.list = [self.f, self.g, self.m, self.s]
 
     def Record(self, right: bool):
         """record the score"""
@@ -57,10 +58,9 @@ class Series():
             raise ValueError("position need to be an integer greater than 0")
 
         f = [1,1]
-        if position > 2:
-            while len(f) < position:
-                new = f[-1] + f[-2]
-                f.append(new)
+        while len(f) < position:
+            new = f[-1] + f[-2]
+            f.append(new)
 
         return f[:position]
 
@@ -71,10 +71,9 @@ class Series():
             raise ValueError("position need to be an integer greater than 0")
 
         g = [1]
-        if position > 1:        
-            while len(g) < position:
-                new = g[-1] * 3 - 1
-                g.append(new)
+        while len(g) < position:
+            new = g[-1] * 3 - 1
+            g.append(new)
 
         return g[:position]
 
@@ -85,15 +84,28 @@ class Series():
             raise ValueError("position need to be an integer greater than 0")
 
         a = [-1]
-        if position > 1:
-            while len(a) < position:
-                if a[-1] > 0:
-                    new = (a[-1] + 7) * -1
-                else:
-                    new = (a[-1] - 7) * -1
-                a.append(new)
+        while len(a) < position:
+            if a[-1] > 0:
+                new = (a[-1] + 7) * -1
+            else:
+                new = (a[-1] - 7) * -1
+            a.append(new)
 
         return a[:position]
+
+    def Sum(self, position: int):
+        """position: need to be an integer greater than 0, return first # numbers of the series"""
+
+        if type(position) != int or position < 1:
+            raise ValueError("position need to be an integer greater than 0")
+
+        s = [1]
+        while len(s) < position:
+            n = len(s) + 1
+            new = (1 + n) * n / 2
+            s.append(new)
+
+        return s[:position]
 
 
 def Play(postion: int=6):
